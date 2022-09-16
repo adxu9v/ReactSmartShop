e7f91e8e89d7f7256b8eb1ceaa681097399f3366
jhw <jjsu4852@naver.com>
2022-09-16 21:04:57 +0900

    image

--- src/data.js
+++ src/data.js
@@ -4,7 +4,7 @@ const data = [
       title : "Iphone 13 Pro",
       content : "이게 바로 프로.거대한 도약을 이룬 iPhone 13 Pro. 무슨 일을 하든 전광석화처럼 처리하는 놀라운 속도와 극적으로 새로워진 사진 및 동영상 기능들을 제공합니다. 그것도 딱 좋은 두 가지 사이즈에 말이죠.",
       price : 1499000,
-      image : '/iphone13pro.jpeg'
+      image : `${process.env.PUBLIC_URL}/iphone13pro.jpeg`
     },
   
     {
@@ -12,7 +12,7 @@ const data = [
       title : "Gallaxy Z Fold 3",
       content : "세상이 스마트폰에 바라는 모든 것을 담아, 활짝 펼쳐 선보입니다. 프리미엄 스펙, 초고속 5G1와 내구성까지 갖췄습니다. 보고, 일하고, 즐기는 모든 경험을 전혀 새롭게 바꿔놓을 광활한 대화면이 지금 여기에 그 찬란한 모습을 드러냅니다.플렉스 모드로 갤럭시 Z 폴드3 5G가 혼자서 설 수 있는 독보적 능력을 선보입니다. 대칭적 디자인으로 밸런스를 잡을 수 있기에 가능한 일입니다.2 이제 두 손이 자유로운 스마트폰의 새 시대를 누리세요.",
       price : 1990000,
-      image:'/zFold3.jpeg'
+      image:`${process.env.PUBLIC_URL}/zFold3.jpeg`
     },
   
     {
@@ -20,25 +20,25 @@ const data = [
       title : "Galaxy Z Flip 3",
       content : "접어서 주머니에 쏙. 스키니 진에도 슥. 작은 가방에도 딱. 꺼내어 펼치면 풀스크린 스마트폰으로 플렉스! 원하는 각도로 자유롭게 펼칠 수도 있죠.조금 다른 생각으로 만들어진 폰 같다구요? 네, 맞아요. 바로 갤럭시 Z 플립3 5G. 당신이 어떤 사람인지 세상에 선언해줄 가장 강력한 스마트폰이 될 겁니다.",
       price : 1254000,
-      image: '/galaxyZFlip3.jpeg'
+      image: `${process.env.PUBLIC_URL}/galaxyZFlip3.jpeg`
     },{
       id : 3,
       title : "Galaxy S Ultra 22",
       content : "s펜으로 더욱 강력해진 갤럭시s22Ultra를 만나보세요. 날렵하면서도 섬세한 바디 미러링 디자인으로 섬세하게 빛나는 플로팅 카메라. 새롭지 않으면 혁신이라 불릴 자격이 없기에. 이번에도 새로운 모습으로 당신앞에 섰습니다.",
       price : 1300000,
-      image : '/galaxyS22Ultra.jpeg'
+      image : `${process.env.PUBLIC_URL}/galaxyS22Ultra.jpeg`
     },{
       id : 4,
       title : "Iphone 13",
       content : "전광석화 같은 칩, 획기적인 배터리 성능 그리고 완전히 새로운 사진 및 동영상 촬영 역량까지. iPhone 13은 지금껏 할 수 없던 일을 가능케하는 능력을 자랑합니다. 그것도 딱 좋은 두 가지 사이즈에 말이죠.",
       price : 999000,
-      image : '/iphone13.jpeg'
+      image : `${process.env.PUBLIC_URL}/iphone13.jpeg`
     },{
       id : 5,
       title : "Ipad Pro 4 12.9",
       content : "Born in the States",
       price : 1400000,
-      image : '/ipad_pro_12.9.jpeg'
+      image : `${process.env.PUBLIC_URL}/ipad_pro_12.9.jpeg`
 
     }
   ] 
